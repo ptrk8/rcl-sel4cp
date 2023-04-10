d3b91f8c7711cdfb15e90a9fb627bfeea4771b74
Pablo Garrido <pablogs9@gmail.com>
2023-04-10 07:27:53 +0200

    micro-ROS Rolling patch
    * Avoid logging on build time based on RCutils flags (#6)
    
    * Add RCL_YAML_PARSER and RCL_LOGGING CMake options.
    
    * Replace RCL_YAML_PARSER by RCL_COMMAND_LINE_ENABLED and RCL_LOGGING by RCL_LOGGING_ENABLED.
    
    * Disabled logging fix
    
    * Fix remapping (#7)
    
    Co-authored-by: Pablo Garrido <pablogs9@gmail.com>
    Co-authored-by: julibert <julianbermudez@eprosima.com>
    
    Updates 17092020
    
    *Release micro-ROS Foxy (#9)
    
    Cleaning
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Add workflow checker
    Update
    
    
    Update
    
    
    update
    
    
    Test
    
    
    Test
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    New approach
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Update
    
    
    Disable logger name (#11) (#13)
    
    (cherry picked from commit 1fac9bf845f32bce311730d3dda1fddbb8b101e6)
    
    Co-authored-by: Pablo Garrido <pablogs9@gmail.com>
    
    Avoid uint64_t operation when handling context (#16) (#18)
    
    * Update
    
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>
    
    Update
    
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>
    
    Update
    
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>
    
    Update
    
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>
    
    * Update
    
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>
    
    * Clean PR
    
    * Clean id rollover check
    
    * Fix rollover
    
    Co-authored-by: acuadros95 <acuadros1995@gmail.com>
    (cherry picked from commit 32aa288817ee54473213ec6d4c6c075c0135c682)
    
    Co-authored-by: Pablo Garrido <pablogs9@gmail.com>
    
    Update fork checker for Humble
    
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>
    
    Update micro-ROS conditional compilation

--- rcl/src/rcl/node.c
+++ rcl/src/rcl/node.c
@@ -24,12 +24,16 @@ extern "C"
 #include <stdlib.h>
 #include <string.h>
 
+#ifdef RCL_MICROROS_COMPLETE_IMPL
 #include "rcl/arguments.h"
+#endif // RCL_MICROROS_COMPLETE_IMPL
 #include "rcl/error_handling.h"
 #include "rcl/init_options.h"
 #include "rcl/localhost.h"
+#ifdef RCL_MICROROS_COMPLETE_IMPL
 #include "rcl/logging.h"
 #include "rcl/logging_rosout.h"
+#endif // RCL_MICROROS_COMPLETE_IMPL
 #include "rcl/rcl.h"
 #include "rcl/remap.h"
 #include "rcl/security.h"
@@ -214,6 +218,7 @@ rcl_node_init(
     goto fail;
   }
 
+#ifdef RCL_MICROROS_COMPLETE_IMPL
   // Remap the node name and namespace if remap rules are given
   rcl_arguments_t * global_args = NULL;
   if (node->impl->options.use_global_arguments) {
@@ -240,6 +245,7 @@ rcl_node_init(
     should_free_local_namespace_ = true;
     local_namespace_ = remapped_namespace;
   }
+#endif // RCL_MICROROS_COMPLETE_IMPL
 
   // compute fully qualfied name of the node.
   if ('/' == local_namespace_[strlen(local_namespace_) - 1]) {
@@ -249,9 +255,11 @@ rcl_node_init(
   }
 
   // node logger name
+#ifdef RCL_MICROROS_COMPLETE_IMPL
   node->impl->logger_name = rcl_create_node_logger_name(name, local_namespace_, allocator);
   RCL_CHECK_FOR_NULL_WITH_MSG(
     node->impl->logger_name, "creating logger name failed", goto fail);
+#endif // RCL_MICROROS_COMPLETE_IMPL
 
   RCUTILS_LOG_DEBUG_NAMED(
     ROS_PACKAGE_NAME, "Using domain ID of '%zu'", context->impl->rmw_context.actual_domain_id);
@@ -284,6 +292,7 @@ rcl_node_init(
     // error message already set
     goto fail;
   }
+#ifdef RCL_MICROROS_COMPLETE_IMPL
   // The initialization for the rosout publisher requires the node to be in initialized to a point
   // that it can create new topic publishers
   if (rcl_logging_rosout_enabled() && node->impl->options.enable_rosout) {
@@ -293,6 +302,7 @@ rcl_node_init(
       goto fail;
     }
   }
+#endif // RCL_MICROROS_COMPLETE_IMPL
   if (node->impl->options.enable_type_description_service) {
     RCUTILS_LOG_WARN_NAMED(
       ROS_PACKAGE_NAME,
@@ -310,6 +320,7 @@ rcl_node_init(
   goto cleanup;
 fail:
   if (node->impl) {
+#ifdef RCL_MICROROS_COMPLETE_IMPL
     if (rcl_logging_rosout_enabled() &&
       node->impl->options.enable_rosout &&
       node->impl->logger_name)
@@ -320,6 +331,7 @@ fail:
         ROS_PACKAGE_NAME, "Failed to fini publisher for node: %i", ret);
       allocator->deallocate((char *)node->impl->logger_name, allocator->state);
     }
+#endif // RCL_MICROROS_COMPLETE_IMPL
     if (node->impl->fq_name) {
       allocator->deallocate((char *)node->impl->fq_name, allocator->state);
     }
@@ -342,6 +354,8 @@ fail:
       }
       allocator->deallocate(node->impl->graph_guard_condition, allocator->state);
     }
+
+#ifdef RCL_MICROROS_COMPLETE_IMPL
     if (NULL != node->impl->options.arguments.impl) {
       ret = rcl_arguments_fini(&(node->impl->options.arguments));
       if (ret != RCL_RET_OK) {
@@ -351,6 +365,7 @@ fail:
         );
       }
     }
+#endif // RCL_MICROROS_COMPLETE_IMPL
     allocator->deallocate(node->impl, allocator->state);
   }
   *node = rcl_get_zero_initialized_node();
@@ -380,6 +395,7 @@ rcl_node_fini(rcl_node_t * node)
   rcl_allocator_t allocator = node->impl->options.allocator;
   rcl_ret_t result = RCL_RET_OK;
   rcl_ret_t rcl_ret = RCL_RET_OK;
+#ifdef RCL_MICROROS_COMPLETE_IMPL
   if (rcl_logging_rosout_enabled() && node->impl->options.enable_rosout) {
     rcl_ret = rcl_logging_rosout_fini_publisher_for_node(node);
     if (rcl_ret != RCL_RET_OK && rcl_ret != RCL_RET_NOT_INIT) {
@@ -387,6 +403,7 @@ rcl_node_fini(rcl_node_t * node)
       result = RCL_RET_ERROR;
     }
   }
+#endif // RCL_MICROROS_COMPLETE_IMPL
   rmw_ret_t rmw_ret = rmw_destroy_node(node->impl->rmw_node_handle);
   if (rmw_ret != RMW_RET_OK) {
     RCL_SET_ERROR_MSG(rmw_get_error_string().str);
@@ -401,12 +418,14 @@ rcl_node_fini(rcl_node_t * node)
   // assuming that allocate and deallocate are ok since they are checked in init
   allocator.deallocate((char *)node->impl->logger_name, allocator.state);
   allocator.deallocate((char *)node->impl->fq_name, allocator.state);
+#ifdef RCL_MICROROS_COMPLETE_IMPL
   if (NULL != node->impl->options.arguments.impl) {
     rcl_ret_t ret = rcl_arguments_fini(&(node->impl->options.arguments));
     if (ret != RCL_RET_OK) {
       return ret;
     }
   }
+#endif // RCL_MICROROS_COMPLETE_IMPL
   allocator.deallocate(node->impl, allocator.state);
   node->impl = NULL;
   RCUTILS_LOG_DEBUG_NAMED(ROS_PACKAGE_NAME, "Node finalized");
